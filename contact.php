73c8398334aa6a18604056f9979323b9d7bc1450
Long <longasau@gmail.com>
2016-03-17 22:03:15 +0200

    add rest of the files

---
+++ contact.php
@@ -0,0 +1,26 @@
+<?php
+header("application/javascript");
+
+$request = $_POST;
+$inputfields = array("form-sender-name", "form-email", "form-message");
+$sendParams = array();
+
+foreach ($inputfields as $field) {
+  
+  if(!isset($_POST[$field]) || empty($_POST[$field])) $error = true;
+  $sendParams[$field] = $_POST[$field];
+
+}
+if ($error) { 
+ echo json_encode(array("status" => "Missing fields!"));
+ exit();
+
+}else { 
+  include("lib/mailer.php");
+  sendMail($sendParams["form-email"], $sendParams["form-sender-name"], $sendParams["form-message"]);
+echo json_encode(array("status" => "Ok!"));
+}
+
+
+
+?>
---
+++ js/main.js
@@ -0,0 +1,109 @@
+$(function()
+{
+			// ACTIVITY INDICATOR
+			// CLOSE BUTTON
+
+			var closeButtonOn = function( instance )
+			{
+				$( '<button type="button" id="imagelightbox-close" title="Close"></button>' ).appendTo( 'body' ).on( 'click touchend', function(){ $( this ).remove(); instance.quitImageLightbox(); return false; });
+			},
+			closeButtonOff = function()
+			{
+				$( '#imagelightbox-close' ).remove();
+			},
+
+			activityIndicatorOn = function()
+			{
+				$( '<div id="imagelightbox-loading"><div></div></div>' ).appendTo( 'body' );
+			},
+			activityIndicatorOff = function()
+			{
+				$( '#imagelightbox-loading' ).remove();
+			},
+
+			// CAPTION
+
+			captionOn = function()
+			{
+				var description = $( 'a[href="' + $( '#imagelightbox' ).attr( 'src' ) + '"] img' ).attr( 'alt' );
+				if( description.length > 0 )
+					$( '<div id="imagelightbox-caption">' + description + '</div>' ).appendTo( 'body' );
+			},
+			captionOff = function()
+			{
+				$( '#imagelightbox-caption' ).remove();
+			},
+
+			// OVERLAY
+
+			overlayOn = function()
+			{
+				$( '<div id="imagelightbox-overlay"></div>' ).appendTo( 'body' );
+			},
+			overlayOff = function()
+			{
+				$( '#imagelightbox-overlay' ).remove();
+			};
+
+			var lightbox = $( 'a.expand' ).imageLightbox(
+			{
+				onStart: 	 function() { overlayOn(); closeButtonOn(lightbox);},
+				onEnd:	 	 function() { closeButtonOff(); overlayOff(); activityIndicatorOff(); captionOff();},
+				onLoadStart: function() { activityIndicatorOn(); captionOff();},
+				onLoadEnd:	 function() { activityIndicatorOff(); captionOn();}
+			});
+
+			$('[data-toggle="tooltip"]').tooltip();
+
+			function mailSuccess(el) {
+			    $(el).parent().fadeOut();
+			    $('#success-contact-message').fadeIn();
+			}
+
+			function mailError(el) { 
+			    $('#contact-form-warning').fadeIn(200);
+			    }
+
+			$('a[rel]').click(function(){ 
+
+				if ($(this).hasClass('navbar-brand')) {
+
+					$("ul.nav > li.active").removeClass("active");
+				}
+				else
+				$(this).parent().addClass("active").siblings().removeClass("active");  
+				
+				var pageId = "#"+$(this).attr("rel");  
+				$('div.page'+pageId).siblings().fadeOut(200).end().fadeIn(800);  
+				$("button[aria-expanded=true]").click();
+
+			    });
+
+			$('form').on('submit', function(e) { 
+				e.preventDefault();
+				var data = $(this).serialize();
+
+				$.post("/contact.php", data, function(res) { 
+					var response = $.parseJSON(res);
+					if (response.status == "Ok!") {  
+					    mailSuccess(this);
+					} else {
+					    mailError(this);
+					}
+
+				    }.bind(this));
+				return false;
+			    });
+
+			var address = window.location.hash.replace("#","");
+			if(address.length > 0){
+			$('a[rel=' + address + ']').click();
+			}
+
+			window.onhashchange = function() {   
+				var address = window.location.hash;
+				$('a[rel=' + address.replace("#","") + ']').click();
+			};
+
+
+});
